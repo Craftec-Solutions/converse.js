f09cffd1e810d7fbaa3035d1a50292cf275e6c6e
igor <igor.craftec@gmail.com>
2026-04-20 12:05:27 +0200

    [chat] Never display jid [chat] Show group chat participants

--- src/plugins/muc-views/modals/templates/muc-details.js
+++ src/plugins/muc-views/modals/templates/muc-details.js
@@ -2,7 +2,18 @@ import { __ } from 'i18n';
 import { html } from "lit";
 
 export default (model) => {
-    const num_occupants = model.occupants.filter((o) => o.get('show') !== 'offline').length;
+    const num_occupants = (() => {
+      const seen = new Set();
+      return model.occupants.filter((o) => {
+        if (o.get('show') === 'offline') return false;
+        const jid = o.get('jid');
+        if (!jid) return false;
+        const bareJid = jid.split('/')[0];
+        if (seen.has(bareJid)) return false;
+        seen.add(bareJid);
+        return true;
+      }).length;
+    })();
 
     const i18n_name = __('Name');
     const i18n_online_users = __('Online users');
@@ -18,8 +29,8 @@ export default (model) => {
     		></converse-avatar>
 
     		<div>
-    			<p><strong>${i18n_name}:</strong> ${model.get('name')}</p>
-    			<p><strong>${i18n_online_users}:</strong> ${num_occupants}</p>
+    			<p style="margin-bottom: 0;"><strong>${i18n_name}:</strong> ${model.get('name')}</p>
+    			<p style="margin-bottom: 0;"><strong>${i18n_online_users}:</strong> ${num_occupants}</p>
     		</div>
     	</div>
   `;
